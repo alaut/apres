3317c4ec4623e4e7f12f465a6ab272fd21fa04a0
Alexander Laut <14655890+alaut@users.noreply.github.com>
2021-11-30 11:42:33 +0100

    include regular rounded polygon

--- apres/shapes.py
+++ apres/shapes.py
@@ -84,3 +84,67 @@ class IrregularRoundedPolygon():
             ind += ind_d
 
         return {'R': r, 'drifts': ind}
+
+@dataclass
+class RegularRoundedPolygon:
+    R: 100
+    rho: 70
+    n: 5
+    dphi: float = 0
+    verbose: bool = False
+    # def rounded_polygon(R, rho, n, verbose=False, dphi=0):
+    """return r(phi) for rounded polygon
+
+    inputs:
+        R: characteristic radius
+        rho: corner radius
+        n: number of sides
+
+    optional:
+        dphi: polar offset
+        verbose: print statements
+
+    outputs:
+        r: function handle returning radius as a function of angle
+        ind: drift section indices"""
+
+    def __post__init__(self):
+
+        alpha = 2*np.pi/n  # sector angle
+
+        L = 2*np.pi*(R-rho)/n  # straight length
+        l = 2*np.pi*rho/n  # bend length
+
+        s = np.sqrt(2*rho**2*(1-np.cos(alpha)))  # bend cord length
+        # cropped corner edge length
+        b = np.abs(s*np.sin(alpha/2)/np.sin(alpha))
+
+        h = (b+L/2)/np.tan(alpha/2)
+
+        gamma = 2*np.arctan(L/(2*h))    # drift angle
+        beta = alpha-gamma  # bend angle
+
+        delta = (alpha-beta)/2
+        k = rho*np.sin(delta)/np.sin(beta/2)
+
+        if verbose:
+            print('L: {:0.2f}'.format(L))
+            print('l: {:0.2f}'.format(l))
+            print('alpha: {:0.2f} deg'.format(np.degrees(alpha)))
+            print('beta:  {:0.2f} deg'.format(np.degrees(beta)))
+            print('gamma: {:0.2f} deg'.format(np.degrees(gamma)))
+
+    def radius(phi):
+        psi = (phi + dphi) % alpha  # subsector angle
+
+        theta = psi-gamma/2
+        vphi = psi-beta/2-gamma
+
+        r_drift = h/np.cos(theta)
+        r_bend = k*np.cos(vphi) + np.sqrt(k**2*np.cos(vphi)**2 - k**2+rho**2)
+
+        r_drift[np.isnan(r_drift)] = 0
+        r_bend[np.isnan(r_bend)] = 0
+        r = r_drift*(psi <= gamma) + r_bend*(psi > gamma)
+
+        return r, psi <= gamma
